53ffcc89cf11cb4f64a7d9aab8e9fbdfda3d8bd3
vladislavorehov55 <vladislavorehov55@gmail.com>
2019-05-31 18:12:40 +0300

    До 31_05_19

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Login1()
+        {
+            return View();
+        }
+        public ActionResult login1_calendar()
+        {
+            return View();
+        }
+        public ActionResult login1_documents()
+        {
+            return View();
+        }
+        public ActionResult login1_plan()
+        {
+            return View();
+        }
+        public ActionResult login1_report()
+        {
+            return View();
+        }
+        public ActionResult Login2()
+        {
+            return View();
+        }
+        public ActionResult login2_calendar()
+        {
+            return View();
+        }
+        public ActionResult login2_documents()
+        {
+            return View();
+        }
+        public ActionResult login2_plan()
+        {
+            return View();
+        }
+        public ActionResult login2_report()
+        {
+            return View();
+        }
+        public void Download()
+        {
+            
+        }
+    }
+
+}
---
+++ WebApplication3/Scripts/script.js
@@ -0,0 +1,12 @@
+﻿var login = document.querySelector(".js-login");
+var password = document.querySelector(".js-password");
+var auto_button = document.querySelector(".authorization__button");
+var auto_error = document.querySelector(".authorization__error");
+
+auto_button.addEventListener("click", function (evt) {
+    auto_error.classList.remove("error-show");
+    if ((login.value != "ivnek") || (password.value != "111")) {
+        evt.preventDefault();
+        auto_error.classList.add("error-show");
+    }
+});
